59519dfeeadb37018888f6a6c85cc009d1d47fb0
MONICAMALHOTRA777 <31340079+MONICAMALHOTRA777@users.noreply.github.com>
2017-08-28 17:39:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('article 1',function(req,res){
+app.get('article one',function(req,res){
     res.send('article 1 is requested and will be served here');
 });
-app.get('article 2',function(req,res){
+app.get('article two',function(req,res){
     res.send('article 2 is requested and will be served here');
 });
-app.get('article 3',function(req,res){
+app.get('article three',function(req,res){
     res.send('article 3 is requested and will be served here');
 });
 
